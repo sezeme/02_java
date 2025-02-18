068262e385031174d5f7999b58381a08e87b2177
sezeme <stellaa223@gmail.com>
2025-02-18 15:15:52 +0900

    실습 문제 이동

--- chap01-literal-and-variable-lecture-source/src/main/java/com/sezeme/practice/level02_normal/Application1.java
+++
@@ -1,8 +0,0 @@
-package com.sezeme.practice.level02_normal;
-
-public class Application1 {
-    public static void main(String[] args) {
-        char ch = 'a';
-        System.out.println("문자 a의 unicode : "+ (int)ch);
-    }
-}
